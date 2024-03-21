98ebe227ef81c9c3ece5e37aa06267f890f8f46f
maxi1805 <maxi1805@Maxi.localdomain>
2024-03-20 21:18:57 -0300

    fix: delete console.log

--- parsers/converters.js
+++ parsers/converters.js
@@ -3,8 +3,12 @@ import {headingsTransform} from "./headerParser.js"
 
 const codeTransform = (text) => {
   return text.split("``").map((line, index) => {
-    return index % 2 === 1 ? `<code>${line}</code>`.replace(/<p>/g, "").replace(/<\/p>/g, "")
-    : line.replace(/`([^`]*)`/g, "<code>$1</code>");
+    return index % 2 === 1 ?
+      `<code>${line}</code>`
+        .replace(/<p>/g, "")
+        .replace(/<\/p>/g, "")
+    : line
+      .replace(/`([^`]*)`/g, "<code>$1</code>");
   }).join('');
 }
 
@@ -15,7 +19,3 @@ const listHeadersTransform = (text) => {
 export const convertMarkdonwToHtml = (text) => {
   return codeTransform(formatTransform(listHeadersTransform(text)));
 }
-
-const text = "# Sample Markdown\n\nThis is some basic, sample markdown.\n\n## Second Heading\n\n * Unordered lists, and:\n  1. One\n  1. Two\n  1. Three\n * More\n\n> Blockquote\n\nAnd **bold**, *italics*, and even *italics and later **bold***. Even ~~strikethrough~~. [A link](https://markdowntohtml.com) to somewhere.\n\nAnd code highlighting:\n\n``js\nvar foo = 'bar';\n\nfunction baz(s) {\n   return foo + ':' + s;\n}\n``\n\nOr inline code like `var foo = 'bar';`.\n\nOr an image of bears\n\n![bears](http://placebear.com/200/200)\n\nThe end ..."
-
-console.log(convertMarkdonwToHtml(text));
